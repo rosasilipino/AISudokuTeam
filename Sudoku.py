0074aa2d94721412d4d0206ba815483bb9787af7
rosasilipino <68527568+rosasilipino@users.noreply.github.com>
2024-04-17 05:02:16 +0000

    Base files pulled from DipanshuShukla.

---
+++ Sudoku.py
@@ -0,0 +1,107 @@
+import pygame
+import solver
+import copy
+
+pygame.init()
+
+WIN_WIDTH = 800
+
+SCALE = WIN_WIDTH // 9
+WIN_HEIGHT = WIN_WIDTH + SCALE//2
+
+WINDOW = pygame.display.set_mode((WIN_WIDTH, int(WIN_HEIGHT)))
+pygame.display.set_caption("Sudoku")
+
+STAT_FONT = pygame.font.SysFont("Times", int(SCALE//1.3))
+LABEL_FONT = pygame.font.SysFont("Century ", int(SCALE//2.5))
+
+# Color Pallet
+BLACK = [72, 72, 72]
+GREY = [100, 100, 100]
+TEXT = [160, 160, 160]
+ITEXT = [87, 137, 217]
+WHITE = [255, 255, 255]
+
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.initBoard = []
+
+    def Pick_Board(self):
+        self.initBoard = [[0, 0, 0, 0, 0, 0, 0, 0, 8],
+        [0, 2, 0, 0, 5, 0, 7, 6, 0],
+        [0, 6, 0, 0, 0, 0, 0, 0, 3],
+        [5, 0, 0, 0, 0, 0, 2, 0, 7],
+        [0, 3, 0, 0, 1, 0, 0, 0, 0],
+        [2, 0, 0, 4, 0, 0, 0, 3, 0],
+        [0, 0, 0, 6, 0, 0, 0, 0, 0],
+        [8, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 2, 7, 0, 0, 4, 0]]
+
+        self.board = copy.deepcopy(self.initBoard)
+
+    def Get_Board(self):
+        return self.board
+
+    def Draw(self):
+        WINDOW.fill(WHITE)
+        for x in range(0, WIN_WIDTH, SCALE):
+            pygame.draw.line(WINDOW, GREY, (x, 0), (x, WIN_WIDTH), 4)
+            pygame.draw.line(WINDOW, GREY, (0, x), (WIN_WIDTH, x), 4)
+
+        for x in range(0, WIN_WIDTH + 1, WIN_WIDTH // 3 - 1):
+            pygame.draw.line(WINDOW, BLACK, (x, 0), (x, WIN_WIDTH), 8)
+            pygame.draw.line(WINDOW, BLACK, (0, x), (WIN_WIDTH, x), 8)
+
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                text = STAT_FONT.render("{}".format(self.board[row][col] if self.board[row][col] > 0 else ""), 1, TEXT)
+                Wgap = (SCALE - text.get_width()) // 2
+                Hgap = (SCALE - text.get_height()) // 2
+                WINDOW.blit(text, (col * SCALE + Wgap + 2, row * SCALE + Hgap+3))
+
+        for row in range(len(self.initBoard)):
+            for col in range(len(self.initBoard[row])):
+                text = STAT_FONT.render("{}".format(self.initBoard[row][col] if self.initBoard[row][col] > 0 else ""), 1, ITEXT)
+                Wgap = (SCALE - text.get_width()) // 2
+                Hgap = (SCALE - text.get_height()) // 2
+                WINDOW.blit(text, (col * SCALE + Wgap + 2, row * SCALE + Hgap+3))
+
+        text = LABEL_FONT.render("{}".format("Press SPACE to solve automatically."), 1, BLACK)
+        WINDOW.blit(text, (int(WIN_WIDTH//2 - text.get_width()//2), int(WIN_HEIGHT-SCALE//2.7)))
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+
+def main():
+    FPS = 30
+    Clock = pygame.time.Clock()
+    run = True
+
+    board = Board()
+    board.Pick_Board()
+
+    while run:
+        Clock.tick(FPS)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            solver.Solve(board)
+
+        board.Draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+if __name__ == '__main__':
+    main()
