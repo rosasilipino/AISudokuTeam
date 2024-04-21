ce50aee417f7d4606e736bc654ca0f7a8a121c0a
rosasilipino <68527568+rosasilipino@users.noreply.github.com>
2024-04-21 13:57:06 -0700

    Added Header and comments.

--- chooseLevel.py
+++ chooseLevel.py
@@ -1,6 +1,12 @@
+#Group: Sudoku Team
+#Class: CS 450-01
+#Project: Final Project
+#Date: 04/21/2024
+
 import pygame
+import sys
 
-
+# Color pallet.
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -9,7 +15,8 @@ RED = (255, 0, 0)
 L_RED = (255, 204, 203)
 GRAY = (80, 80, 80)
 YELLOW = (255, 255, 0)
-#
+
+# Initialize window for pygame.
 pygame.init()
 X = 700
 Y = 700
@@ -17,8 +24,7 @@ size = (X, Y)
 window = pygame.display.set_mode(size)
 font = pygame.font.Font('freesansbold.ttf', 25)
 
-
-
+# Function to draw buttons on the screen.
 def drawButton(window, left, top, width, height, color, textInButton, font,  hover=False):
     rectSize = pygame.Rect(left, top, width, height)
     if hover:
@@ -34,12 +40,11 @@ def drawButton(window, left, top, width, height, color, textInButton, font,  hov
     fontButton = pygame.font.Font('freesansbold.ttf', 20)
     textButton = fontButton.render(textInButton, True, BLACK)
     
-    textRectButton = textButton.get_rect()
-    
-    
+    textRectButton = textButton.get_rect()  # Get text rectangle
     textRectButton.center = rectSize.center  # Center text
-    window.blit(textButton, textRectButton)
+    window.blit(textButton, textRectButton) # Draw text on button
 
+# Function to choose the level of difficulty.
 def chooseLevel(window):
     level = 0 # Initialize level to 0
     fontButton = pygame.font.Font('freesansbold.ttf', 20) # Font for buttons
@@ -49,6 +54,7 @@ def chooseLevel(window):
     textRect = text.get_rect() # Get text rectangle
     textRect.center = (window.get_width() // 2, 300) # Center text
     
+    # Loop for event handling for program exiting and input based of user clicks. 
     done = True
     while done:
         window.fill(GRAY)
@@ -62,10 +68,14 @@ def chooseLevel(window):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = False
+                print("EXITING PROGRAM, X WAS PRESSED.")
+                pygame.quit()
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    done = False
+                    print("EXITING PROGRAM ESC WAS PRESSED.")
+                    pygame.quit()
+                    sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Check if the click is within any button's rectangle
                 if 225 <= pos[0] <= 285 and 340 <= pos[1] <= 370:
@@ -74,8 +84,9 @@ def chooseLevel(window):
                     level = 2
                 elif 425 <= pos[0] <= 485 and 340 <= pos[1] <= 370:
                     level = 3
-                if level != 0:
-                    return level  # Return level without quitting pygame
+                else:
+                    print("INVALID LEVEL DEFAULT TO EASY")
+                    level = 1
                 return level  # Return level without quitting pygame
         pygame.display.update()
     
